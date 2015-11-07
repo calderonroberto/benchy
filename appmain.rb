9205eb07b79afef5cbd2f126e5f5ecb3d4987b4d
Roberto <roberto@robertocalderon.ca>
2015-11-07 15:18:35 -0800

    added Benchy endpoints

--- appmain.rb
+++ appmain.rb
@@ -13,3 +13,28 @@ get '/transactions' do
   content_type :json
   benchy.transactions.to_json
 end
+
+get '/balance' do
+  benchy = settings.benchy
+  benchy.get_data
+
+  content_type :json
+  {:balance => benchy.compute_balance}.to_json
+end
+
+
+get '/categories' do
+  benchy = settings.benchy
+  benchy.get_data
+
+  content_type :json
+  benchy.get_category_expenses.to_json
+end
+
+get '/balances' do
+  benchy = settings.benchy
+  benchy.get_data
+
+  content_type :json
+  benchy.get_daily_balances.to_json
+end
